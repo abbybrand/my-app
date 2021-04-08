f364d8b0675b5ec59e83c341514666a0e08ea49c
abbybrand <59895591+abbybrand@users.noreply.github.com>
2021-04-08 18:16:56 -0500

    WOOOOOO

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Abby Brand is so good at web dev! <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
